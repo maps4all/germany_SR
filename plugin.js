d85a7e9b8cf799cdc50d440317de484946f23f79
maps4all <maps4all007@yandex.com>
2017-08-08 22:57:01 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,10 @@
+{
+  "moduleId" : "grabowCommuter/germany_SR",
+  "type" : "Website",
+  "name" : "Saarland",
+  "uri" : "http://www.sr.de/sr/home/nachrichten/verkehr/verkehr110.html",
+  "startTrigger" : "Verkehrslage",
+  "endTrigger" : "Aktualisierung",
+  "startTag" : "b",
+  "endTag" : "span"
+}
